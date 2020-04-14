8fdcab45864efef903781ee73858788d1bb6f7fa
gaoyuan <707216098@qq.com>
2020-04-15 00:46:45 +0800

    pod

--- ForiOS/ForiOS/AppDelegate.m
+++ ForiOS/ForiOS/AppDelegate.m
@@ -37,5 +37,31 @@
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [UIApplication sharedApplication].idleTimerDisabled = NO;
+    
+    //socek ----  申请时间资源
+    UIApplication* app = [UIApplication sharedApplication];
+    __block  UIBackgroundTaskIdentifier bgTask;
+    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (bgTask != UIBackgroundTaskInvalid) {
+                bgTask = UIBackgroundTaskInvalid;
+            }
+        });
+    }];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (bgTask != UIBackgroundTaskInvalid) {
+                bgTask = UIBackgroundTaskInvalid;
+            }
+        });
+    });
+    
+    //实现一个可以后台运行几分钟的权限, 当用户在后台强制退出程序时就会走applicationWillTerminate 了.
+    [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:nil];
+}
+
 
 @end
