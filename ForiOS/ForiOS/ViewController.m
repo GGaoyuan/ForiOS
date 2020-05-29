a44c560de4f8a789756170690c49de1b9a93c95c
gaoyuan <yuan.gao@ivydad.com>
2020-05-29 11:37:09 +0800

    add

--- ForiOS/ForiOS/ViewController.m
+++ ForiOS/ForiOS/ViewController.m
@@ -14,16 +14,7 @@
 #import "AssignSubObject.h"
 @interface ViewController ()
 
-@property (nonatomic, assign) NSInteger testNum;
-@property (nonatomic, assign) AssignObject *assignObject;
-
-@property (nonatomic, weak) id object;
-
-@property (nonatomic, weak) NSObject *testWeak;
-@property (nonatomic, assign) NSObject *testAssign;
-
-@property (nonatomic, copy) NSString *testStr;
-@property (nonatomic, copy) NSMutableArray *testArray;
+@property (nonatomic, assign) NSInteger result;
 
 @end
 
@@ -32,62 +23,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    dispatch_queue_t queue = dispatch_queue_create("id", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue = dispatch_queue_create("CONCURRENT", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{ NSLog(@"111 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"222 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"333 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"444 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"555 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"666 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"777 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue, ^{ NSLog(@"888 ---- %@", [NSThread currentThread]);});
     
-    dispatch_async(queue, ^{
-        for (int i = 0; i < 1000000; i++) {
-            if (i == 999990) {
-                NSLog(@"1111 ---- %@", [NSThread currentThread]);
-            }
-        }
-    });
-    dispatch_async(queue, ^{
-        for (int i = 0; i < 10000000; i++) {
-            if (i == 9999900) {
-                NSLog(@"2222 ---- %@", [NSThread currentThread]);
-            }
-        }
-    });
-    dispatch_async(queue, ^{
-        for (int i = 0; i < 1000000; i++) {
-            if (i == 999990) {
-                NSLog(@"3333 ---- %@", [NSThread currentThread]);
-            }
-        }
-    });
-    dispatch_sync(queue, ^{
-        for (int i = 0; i < 1000000; i++) {
-            if (i == 909999) {
-                NSLog(@"4444 ---- %@", [NSThread currentThread]);
-            }
-        }
-    });
-    dispatch_sync(queue, ^{
-        for (int i = 0; i < 100000; i++) {
-            if (i == 99999) {
-                NSLog(@"5555 ---- %@", [NSThread currentThread]);
-            }
-        }
-    });
+//    dispatch_queue_t queue2 = dispatch_queue_create("CONCURRENT2", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_async(queue2, ^{ NSLog(@"~~~111 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~222 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~333 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~444 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~555 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~666 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~777 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue2, ^{ NSLog(@"~~~888 ---- %@", [NSThread currentThread]);});
     NSLog(@"finish");
-//    NSString *url = @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1589985186178&di=d9bd6c8a6debf28bc3607aaa5006dd5e&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinakd20200416ac%2F185%2Fw640h345%2F20200416%2Fdddf-iskepxs4936582.jpg";
-//    UIImageView *imageView = [[UIImageView alloc] init];
-////    imageView.backgroundColor = [UIColor redColor];
-//    imageView.frame = CGRectMake(100, 100, 200, 120);
-//    [self.view addSubview:imageView];
-//    [imageView sd_setImageWithURL:[NSURL URLWithString:url]];
-//    
-//    self.testStr = @"1111";
-//    self.testArray = [[NSMutableArray alloc] init];
-//    [self.testArray addObject:[NSObject new]];
-//    SwiftMain *swift = [SwiftMain new];
-//    [swift enter];
-//
-//    NewDictionary *newDic = [NewDictionary new];
-//    newDic.string = @"11111";
-//
-//    [AssignObject AssignTestMethod];
-//    [AssignSubObject AssignTestMethod];
+}
+
+- (void)readMethod {
+    
+}
+
+- (void)writeMethod {
+    
 }
 
 
