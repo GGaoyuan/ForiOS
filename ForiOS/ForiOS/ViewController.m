1efaa123bffe4f9e35a361a0dbc20cea6213e9e8
gaoyuan <yuan.gao@ivydad.com>
2020-05-29 16:03:15 +0800

    add

--- ForiOS/ForiOS/ViewController.m
+++ ForiOS/ForiOS/ViewController.m
@@ -23,25 +23,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    dispatch_queue_t queue = dispatch_queue_create("CONCURRENT", DISPATCH_QUEUE_CONCURRENT);
-    dispatch_async(queue, ^{ NSLog(@"111 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"222 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"333 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"444 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"555 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"666 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"777 ---- %@", [NSThread currentThread]);});
-    dispatch_async(queue, ^{ NSLog(@"888 ---- %@", [NSThread currentThread]);});
+//    dispatch_queue_t queue = dispatch_queue_create("CONCURRENT", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_async(queue, ^{ NSLog(@"111 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"222 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"333 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"444 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"555 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"666 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"777 ---- %@", [NSThread currentThread]);});
+//    dispatch_async(queue, ^{ NSLog(@"888 ---- %@", [NSThread currentThread]);});
     
-//    dispatch_queue_t queue2 = dispatch_queue_create("CONCURRENT2", DISPATCH_QUEUE_CONCURRENT);
-//    dispatch_async(queue2, ^{ NSLog(@"~~~111 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~222 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~333 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~444 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~555 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~666 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~777 ---- %@", [NSThread currentThread]);});
-//    dispatch_async(queue2, ^{ NSLog(@"~~~888 ---- %@", [NSThread currentThread]);});
+//    dispatch_queue_t queue2 = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    dispatch_queue_t queue2 = dispatch_get_main_queue();
+    dispatch_queue_t queue2 = dispatch_queue_create("CONCURRENT", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue2, ^{
+        NSLog(@"~~~111 ---- %@", [NSThread currentThread]);
+    });
+    
+    dispatch_async(queue2, ^{ NSLog(@"~~~222 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~333 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~444 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~555 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~666 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~777 ---- %@", [NSThread currentThread]);});
+    dispatch_async(queue2, ^{ NSLog(@"~~~888 ---- %@", [NSThread currentThread]);});
     NSLog(@"finish");
 }
 
