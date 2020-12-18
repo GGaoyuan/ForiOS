5586aa9b161c530f7b94bc210efada054b95897b
gaoyuan <707216098@qq.com>
2020-12-18 15:44:28 +0800

    删除storyboard

---
+++ iOS/iOS/iOS/Classes/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  iOS
+//
+//  Created by gaoyuan on 2020/12/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        
+    }
+}
