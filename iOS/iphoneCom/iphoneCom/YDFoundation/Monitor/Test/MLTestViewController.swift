0428431feb2589ad21b343d451cacfdd44892f7d
gaoyuan <707216098@qq.com>
2020-12-21 15:17:34 +0800

    666

---
+++ iOS/iphoneCom/iphoneCom/YDFoundation/Monitor/Test/MLTestViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MLTestViewController.swift
+//  iOS
+//
+//  Created by gaoyuan on 2020/11/26.
+//
+
+import UIKit
+import SnapKit
+
+
+class MLTestViewController: UIViewController {
+    
+    var testView: MLTestView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testView = MLTestView()
+        testView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        testView.backgroundColor = .red
+        view.addSubview(testView)
+        testView.blk = {
+            self.mlFunc()
+        }
+        
+        
+        let detector = FBRetainCycleDetector()
+        detector.addCandidate(self)
+        let result = detector.findRetainCycles()
+        print("")
+    }
+
+    func mlFunc() {
+        
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    deinit {
+        print("deinit MLTestViewController")
+    }
+}
